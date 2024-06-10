d55ce3d07a2c86862d278531a6f18f2a66db746f
joshcordek <josh_cordek1@baylor.edu>
2024-06-10 10:36:05 -0700

    Update FoodClass.py

--- FoodClass.py
+++ FoodClass.py
@@ -6,25 +6,18 @@ class Customer:
         self._email = email
         self._phone = phone
         self._member_status = member_status
-
     def get_customerid(self):
         return self._customerid
-
     def get_name(self):
         return self._name
-
     def get_address(self):
         return self._address
-
     def get_email(self):
         return self._email
-
     def get_phone(self):
         return self._phone
-
     def get_member_status(self):
         return self._member_status
-
 
 class Transaction:
     def __init__(self, date, item_name, cost, customerid):
@@ -32,15 +25,11 @@ class Transaction:
         self._item_name = item_name
         self._cost = cost
         self._customerid = customerid
-
     def get_date(self):
         return self._date
-
     def get_item_name(self):
         return self._item_name
-
     def get_cost(self):
         return self._cost
-
     def get_customerid(self):
         return self._customerid
